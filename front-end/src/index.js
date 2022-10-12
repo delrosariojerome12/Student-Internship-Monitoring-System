32208c20bb59c5bd94e39cbf2bd7deaeccfa3616
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-12 09:45:40 +0800

    added sass variables and redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import "./styles/css/main.css";
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
+import {store} from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
