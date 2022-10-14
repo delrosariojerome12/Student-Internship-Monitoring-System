473e79295f319ca66cc26ae3b9cfa212555f3e1f
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-14 10:34:44 +0800

    added desktop login layout

--- front-end/src/components/auth/login.jsx
+++ front-end/src/components/auth/login.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import img from "../../assets/img/bg.png";
+import logo from "../../assets/img/logo.svg";
+import {FaUserAlt, FaLock} from "react-icons/fa";
+import {IconContext} from "react-icons";
 
 const Login = () => {
-  return <section className="login-container">Login</section>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="login-container">
+      <div className="left-card">
+        <img src={logo} alt="" />
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <IconContext.Provider value={{color: "#f1faee"}}>
+            <label htmlFor="username">
+              <span>
+                <FaUserAlt />
+              </span>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+              />
+            </label>
+            <label htmlFor="password">
+              <span>
+                <FaLock />
+              </span>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </label>
+
+            <div className="options">
+              <label htmlFor="remember-me">
+                <input type="checkbox" name="remeber-me" id="remember-me" />
+                <p>Remember Me</p>
+              </label>
+              <a href="#">Forgot Password</a>
+            </div>
+
+            <button type="submit">Login</button>
+          </IconContext.Provider>
+        </form>
+        <a href="#">Create Account</a>
+      </div>
+      <div className="right-card">
+        <img src={img} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Login;
