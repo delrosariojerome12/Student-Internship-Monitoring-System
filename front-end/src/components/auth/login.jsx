13015c7eefcf8974ebc49ae6c4199bad17dc7067
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-14 11:17:14 +0800

    partially added waves
    responsive problems with waves

--- front-end/src/components/auth/login.jsx
+++ front-end/src/components/auth/login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import img from "../../assets/img/bg.png";
 import logo from "../../assets/img/logo.svg";
+import bgWaves from "../../assets/img/bg-waves.svg";
 import {FaUserAlt, FaLock} from "react-icons/fa";
 import {IconContext} from "react-icons";
 
@@ -9,52 +10,55 @@ const Login = () => {
     e.preventDefault();
   };
   return (
-    <section className="login-container">
-      <div className="left-card">
-        <img src={logo} alt="" />
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-          <IconContext.Provider value={{color: "#f1faee"}}>
-            <label htmlFor="username">
-              <span>
-                <FaUserAlt />
-              </span>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Username"
-              />
-            </label>
-            <label htmlFor="password">
-              <span>
-                <FaLock />
-              </span>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-              />
-            </label>
-
-            <div className="options">
-              <label htmlFor="remember-me">
-                <input type="checkbox" name="remeber-me" id="remember-me" />
-                <p>Remember Me</p>
+    <main className="login">
+      {/* <img className="bg-waves" src={bgWaves} alt="" /> */}
+      <section className="login-container">
+        <div className="left-card">
+          <img src={logo} alt="" />
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <IconContext.Provider value={{color: "#f1faee"}}>
+              <label htmlFor="username">
+                <span>
+                  <FaUserAlt />
+                </span>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                />
               </label>
-              <a href="#">Forgot Password</a>
-            </div>
+              <label htmlFor="password">
+                <span>
+                  <FaLock />
+                </span>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                />
+              </label>
 
-            <button type="submit">Login</button>
-          </IconContext.Provider>
-        </form>
-        <a href="#">Create Account</a>
-      </div>
-      <div className="right-card">
-        <img src={img} alt="" />
-      </div>
-    </section>
+              <div className="options">
+                <label htmlFor="remember-me">
+                  <input type="checkbox" name="remeber-me" id="remember-me" />
+                  <p>Remember Me</p>
+                </label>
+                <a href="#">Forgot Password</a>
+              </div>
+
+              <button type="submit">Login</button>
+            </IconContext.Provider>
+          </form>
+          <a href="#">Create Account</a>
+        </div>
+        <div className="right-card">
+          <img src={img} alt="" />
+        </div>
+      </section>
+    </main>
   );
 };
 
