c65938e1591b10f2fdb61ee4b2e2ab70e028c1a7
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-15 17:31:55 +0800

    mobile and tablet view for login

--- front-end/src/components/auth/login.jsx
+++ front-end/src/components/auth/login.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import img from "../../assets/img/bg.png";
 import logo from "../../assets/img/logo.svg";
-import bgWaves from "../../assets/img/bg-waves.svg";
+// import bgWaves from "../../assets/img/bg-waves.svg";
 import {FaUserAlt, FaLock} from "react-icons/fa";
 import {IconContext} from "react-icons";
 
 const Login = () => {
+  const [hasUsername, setHasUsername] = useState(false);
+  const [hasPassword, setHasPassword] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleOnchange = (value, input) => {
+    switch (input) {
+      case "username":
+        return value ? setHasUsername(true) : setHasUsername(false);
+
+      case "password":
+        return value ? setHasPassword(true) : setHasPassword(false);
+      default:
+    }
   };
   return (
     <main className="login">
@@ -23,6 +37,10 @@ const Login = () => {
                   <FaUserAlt />
                 </span>
                 <input
+                  style={
+                    hasUsername ? {padding: "0.5rem 0.5rem 0.5rem 6rem"} : null
+                  }
+                  onChange={(e) => handleOnchange(e.target.value, e.target.id)}
                   type="text"
                   name="username"
                   id="username"
@@ -34,9 +52,13 @@ const Login = () => {
                   <FaLock />
                 </span>
                 <input
+                  style={
+                    hasPassword ? {padding: "0.5rem 0.5rem 0.5rem 6rem"} : null
+                  }
                   type="password"
                   name="password"
                   id="password"
+                  onChange={(e) => handleOnchange(e.target.value, e.target.id)}
                   placeholder="Password"
                 />
               </label>
@@ -46,13 +68,15 @@ const Login = () => {
                   <input type="checkbox" name="remeber-me" id="remember-me" />
                   <p>Remember Me</p>
                 </label>
-                <a href="#">Forgot Password</a>
+                <a href="https://web.facebook.com/?_rdc=1&_rdr">
+                  Forgot Password
+                </a>
               </div>
 
               <button type="submit">Login</button>
             </IconContext.Provider>
           </form>
-          <a href="#">Create Account</a>
+          <a href="https://web.facebook.com/?_rdc=1&_rdr">Create Account</a>
         </div>
         <div className="right-card">
           <img src={img} alt="" />
