20a0bef31e3efdfe16aa7f13fa9b20ad7da80c2e
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-16 17:51:51 +0800

    path added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Login from "./components/auth/login";
+import Signin from "./components/auth/signin";
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
     <div className="container">
-      <Login />
+      <Routes>
+        <Route path="/account/login" element={<Login />} />
+        <Route path="/account/signin" element={<Signin />} />
+      </Routes>
     </div>
   );
 };
