2350aa0ac3c86f402a273ec6ec14128f7bb98a10
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-18 10:15:53 +0800

    updated login desgin
    added  card-bg

--- front-end/src/components/auth/signin.jsx
+++ front-end/src/components/auth/signin.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Signin = () => {
   return (
     <main className="signin">
-      <div className="signin-container">
-        <h1>Signin</h1>
-        <h1>penge ulam bukas</h1>
-        <h1>testing</h1>
-      </div>
+      <div className="signin-container"></div>
     </main>
   );
 };
