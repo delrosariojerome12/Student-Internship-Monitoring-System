aefddbfd86340e4d007c7f1f79733a236c4ea0c7
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-21 08:58:31 +0800

    update

---
+++ back-end/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const CustomApiError = require("../errors/customError");
+const {StatusCodes} = require("http-status-codes");
+
+const errorHandler = (req, res, err) => {
+  if (err instanceof CustomApiError) {
+    return res.status(err.statusCode).json({success: false, mss: err.message});
+  }
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({success: false, msg: err.message});
+};
+
+module.exports = errorHandler;
