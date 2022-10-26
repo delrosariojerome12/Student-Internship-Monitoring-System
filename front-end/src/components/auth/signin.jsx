c48df38ab5990884e6b1769eebeb53f1c01fd290
fallenZeR0 <delrosario12jerome@gmail.com>
2022-10-26 08:20:56 +0800

    update signin

--- front-end/src/components/auth/signin.jsx
+++ front-end/src/components/auth/signin.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import logo from "../../assets/img/logo.svg";
-import img from "../../assets/img/secure-login.svg";
-import {FaUserAlt, FaLock, FaEye, FaEyeSlash} from "react-icons/fa";
+import {
+  FaUserAlt,
+  FaLock,
+  FaEye,
+  FaEyeSlash,
+  FaCheckCircle,
+} from "react-icons/fa";
+import {GrMail} from "react-icons/gr";
 import {IconContext} from "react-icons";
 import {useState} from "react";
 
@@ -26,7 +33,7 @@ const Signin = () => {
       type: "email",
       id: "email",
       value: "",
-      IconType: FaUserAlt,
+      IconType: GrMail,
     },
     {
       forInput: "Password",
@@ -40,7 +47,7 @@ const Signin = () => {
       type: "password",
       id: "confirm-password",
       value: "",
-      IconType: FaLock,
+      IconType: FaCheckCircle,
     },
   ]);
 
@@ -54,48 +61,49 @@ const Signin = () => {
   };
 
   return (
-    <section className="auth-container signin">
+    <section className="signin">
       <section className="bg-container"></section>
-      <section className="signin card-container">
-        <div className="left-card">
-          <img src={img} alt="bg.img" />
-        </div>
-        <div className="right-card">
+      <section className="card-container">
+        <header>
           <img src={logo} alt="" />
-          <h1>Signin</h1>
-          <form onSubmit={handleSubmit}>
-            <IconContext.Provider value={{color: "#000", className: "icons"}}>
-              {form.map((inputs, index) => {
-                const {forInput, id, type, value, IconType} = inputs;
-                return (
-                  <div className="input-contain" key={index}>
-                    <input
-                      type={type}
-                      name={forInput}
-                      id={id}
-                      value={value}
-                      onChange={(e) => handleOnChange(e.target.value, index)}
-                    />
-                    <label
-                      htmlFor={id}
-                      className={
-                        value ? "placeholder-text active" : "placeholder-text"
-                      }
-                    >
-                      <div className="text">
-                        <span>
-                          <IconType />
-                        </span>
-                        {forInput}
-                      </div>
-                    </label>
-                  </div>
-                );
-              })}
-              <button type="submit">Signin</button>
-            </IconContext.Provider>
-          </form>
-        </div>
+          <h1>Create Your Account</h1>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <IconContext.Provider value={{color: "#000", className: "icons"}}>
+            {form.map((inputs, index) => {
+              const {forInput, id, type, value, IconType} = inputs;
+              return (
+                <div className="input-contain" key={index}>
+                  <input
+                    type={type}
+                    name={forInput}
+                    id={id}
+                    value={value}
+                    onChange={(e) => handleOnChange(e.target.value, index)}
+                  />
+                  <label
+                    htmlFor={id}
+                    className={
+                      value ? "placeholder-text active" : "placeholder-text"
+                    }
+                  >
+                    <div className="text">
+                      <span>
+                        <IconType />
+                      </span>
+                      {forInput}
+                    </div>
+                  </label>
+                </div>
+              );
+            })}
+            <span>
+              <p>Already have an account?</p>
+              <Link to="/account/login">Log in</Link>
+            </span>
+            <button type="submit">Signin</button>
+          </IconContext.Provider>
+        </form>
       </section>
     </section>
   );
