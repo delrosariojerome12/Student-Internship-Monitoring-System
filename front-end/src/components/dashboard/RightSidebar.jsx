47704174fc012db75966a95638e5773e81b19953
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-11-05 18:18:31 +0800

    update

--- front-end/src/components/dashboard/RightSidebar.jsx
+++ front-end/src/components/dashboard/RightSidebar.jsx
@@ -3,9 +3,20 @@ import React from "react";
 const RightSidebar = () => {
   return (
     <aside className="right-sidebar">
-      <div className="chat"></div>
-      <div className="graph"></div>
-      <div className="graph2"></div>
+      <div className="top">
+        <div className="chat">
+          <h1>message</h1>
+        </div>
+        <div className="profile">
+          <h1>profile</h1>
+        </div>
+      </div>
+      <div className="graph">
+        <h1>Graph</h1>
+      </div>
+      <div className="graph2">
+        <h1>Graph 2</h1>
+      </div>
     </aside>
   );
 };
--- front-end/src/pages/dashboard/DashboardMain.jsx
+++ front-end/src/pages/dashboard/DashboardMain.jsx
@@ -4,13 +4,30 @@ const DashboardMain = () => {
   return (
     <section className="main">
       <header>
-        <h2 className="name"></h2>
+        <h1 className="name">Hello, Hakdog</h1>
         <h4>Welcome Back!</h4>
       </header>
       <div className="content">
-        <div className="time-keeper"></div>
-        <div className="documents"></div>
-        <div className="internship-details"></div>
+        <div className="time-keeper">
+          <h4>Time Keeper</h4>
+          <div>
+            <h3>TIME</h3>
+          </div>
+        </div>
+        <div className="documents">
+          <h4>Documents</h4>
+          <div>
+            <h3>TODO</h3>
+          </div>
+        </div>
+        <div className="internship-details">
+          <h4>Intership Details</h4>
+          <div>
+            <h5>Motorpool</h5>
+            <h6>Mahiwagang Hakdog</h6>
+            <h6>666 position</h6>
+          </div>
+        </div>
         <div className="map"></div>
       </div>
     </section>
