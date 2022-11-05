96bdc4a892adac1cd50bf799af3bedca39066043
fallenZeR0 <delrosario12jerome@gmail.com>
2022-11-05 15:44:38 +0800

    update

--- front-end/src/components/dashboard/SidebarLeft.jsx
+++ front-end/src/components/dashboard/SidebarLeft.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { RiDashboardLine } from "react-icons/ri";
-import { HiPencilAlt } from "react-icons/hi";
-import { HiDocument, HiTrendingUp } from "react-icons/hi";
-import { FaUserAlt } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
-import { IconContext } from "react-icons";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {RiDashboardLine} from "react-icons/ri";
+import {HiPencilAlt} from "react-icons/hi";
+import {HiDocument, HiTrendingUp} from "react-icons/hi";
+import {FaUserAlt, FaChevronLeft, FaChevronRight} from "react-icons/fa";
+import {IoMdSettings} from "react-icons/io";
+import {IconContext} from "react-icons";
 
-import logo from "../../assets/img/landingPage/Logo.png";
+import logo from "../../assets/img/logo.svg";
 
 const links = [
   {
@@ -44,27 +44,34 @@ const links = [
 ];
 
 const SidebarLeft = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const handleSideBar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   return (
-    <aside className="left-sidebar">
-      <div className="img-con">
-        <img src={logo} alt="Logo.png " />
-      </div>
-      <div className="links-con">
-        <IconContext.Provider value={{ className: "icons", color: "white" }}>
+    <aside className={isSidebarOpen ? "left-sidebar active" : "left-sidebar"}>
+      <IconContext.Provider value={{className: "icons", color: "white"}}>
+        <div className="img-con">
+          <img src={logo} alt="Logo.png " />
+        </div>
+        <div className="links-con">
+          <span onClick={handleSideBar} className="collapse-icon">
+            {isSidebarOpen ? <FaChevronRight /> : <FaChevronLeft />}
+          </span>
           {links.map((item, index) => {
-            const { path, link, IconType } = item;
+            const {path, link, IconType} = item;
             return (
               <Link to={path} key={index}>
                 <IconType />
-                {link}
+                {!isSidebarOpen && link}
               </Link>
             );
           })}
-        </IconContext.Provider>
-      </div>
-      <div className="box">
-        <button type="submit">Log In</button>
-      </div>
+        </div>
+        <div className="box">
+          <button type="submit">Log In</button>
+        </div>
+      </IconContext.Provider>
     </aside>
   );
 };
