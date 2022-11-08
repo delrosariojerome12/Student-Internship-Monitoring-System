ef185cea58acb65c0cbf718893b30544d4180db5
fallenZeR0 <delrosario12jerome@gmail.com>
2022-11-08 21:17:53 +0800

    update

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import globalReducer from "./features/globalReducer";
 import userReducer from "./features/user/userReducer";
+import dashboard from "./features/dashboard/dashboard";
 
 export const store = configureStore({
   reducer: {
     global: globalReducer,
     user: userReducer,
+    dashboard: dashboard,
   },
 });
