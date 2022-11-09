8d7235a8e9be7bced1f6343fcb3cba60e28d9ca6
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-11-09 22:02:39 +0800

    update

--- front-end/src/pages/dashboard/documents/Completion.jsx
+++ front-end/src/pages/dashboard/documents/Completion.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Completion = () => {
-  return <div>Completion</div>;
+  return (
+    <div className="completion-container">
+      <div className="evaluation-form"></div>
+      <div className="clearance-form"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+    </div>
+  );
 };
 
 export default Completion;
--- front-end/src/pages/dashboard/documents/Requirments.jsx
+++ front-end/src/pages/dashboard/documents/Requirments.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Requirments = () => {
-  return <div>Requirments</div>;
+  return (
+    <div className="requirments-container">
+      <div className="med-cert"></div>
+      <div className="nbi-pol-clearance"></div>
+      <div className="b-cert"></div>
+      <div className="school-clearance"></div>
+      <div className="parents-consent"></div>
+      <div className="resume"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+      <div className="blank"></div>
+    </div>
+  );
 };
 
 export default Requirments;
