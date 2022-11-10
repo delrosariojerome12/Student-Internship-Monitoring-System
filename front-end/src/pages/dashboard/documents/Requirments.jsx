fe0b754700e3023dba61ac02b205729681c64d01
Leerzej03 <Jezreelmenor03@gmail.com>
2022-11-10 13:20:11 +0800

    updated docu

--- front-end/src/pages/dashboard/documents/Requirments.jsx
+++ front-end/src/pages/dashboard/documents/Requirments.jsx
@@ -3,15 +3,33 @@ import React from "react";
 const Requirments = () => {
   return (
     <div className="requirments-container">
-      <div className="med-cert"></div>
-      <div className="nbi-pol-clearance"></div>
-      <div className="b-cert"></div>
-      <div className="school-clearance"></div>
-      <div className="parents-consent"></div>
-      <div className="resume"></div>
-      <div className="blank"></div>
-      <div className="blank"></div>
-      <div className="blank"></div>
+      <div className="med-cert">
+        <h5>Medical Certificate</h5>
+      </div>
+      <div className="nbi-pol-clearance">
+        <h5>NBI or Police Clearance</h5>
+      </div>
+      <div className="b-cert">
+        <h5>Birth Certificate</h5>
+      </div>
+      <div className="school-clearance">
+        <h5>School Clearance</h5>
+      </div>
+      <div className="parents-consent">
+        <h5>Parents Consent</h5>
+      </div>
+      <div className="resume">
+        <h5>Resume</h5>
+      </div>
+      <div className="blank">
+        <h5>7</h5>
+      </div>
+      <div className="blank">
+        <h5>8</h5>
+      </div>
+      <div className="blank">
+        <h5>9</h5>
+      </div>
     </div>
   );
 };
