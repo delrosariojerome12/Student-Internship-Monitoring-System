789fc6a0a9e327dddf67c18d4390f725393f756d
fallenZeR0 <delrosario12jerome@gmail.com>
2022-11-10 20:44:54 +0800

    update

--- front-end/src/components/sidebarRight/NotificationTab.jsx
+++ front-end/src/components/sidebarRight/NotificationTab.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotificationTab = () => {
-  return <div className="notification-tab tab">NotificationTab</div>;
+  return (
+    <div className="notification-tab tab">
+      <h1>Notifications</h1>
+      <div className="display">.</div>
+    </div>
+  );
 };
 
 export default NotificationTab;
