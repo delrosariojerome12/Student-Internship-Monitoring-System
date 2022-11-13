4cd36d7db76eb6681ce6b15d0b0eb48aca4099d3
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-11-13 20:21:47 +0800

    update

--- back-end/controllers/interns.js
+++ back-end/controllers/interns.js
@@ -5,15 +5,6 @@ const { BadRequest, NotFound } = require("../errors");
 const updateInfos = async (req, res) => {
   const { email, internshipDetails } = req.body;
 
-  if (
-    !companyname ||
-    !companyaddress ||
-    !contactnumber ||
-    !requiredhours ||
-    !supervisor
-  ) {
-    throw new BadRequest("fields cannot be empty");
-  }
   const intern = await User.findOneAndUpdate({ email }, req.body, {
     new: true,
     runValidators: true,
