009588e6ca474cc9b405860b2c560b2e5a5690b0
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-12-01 19:25:54 +0800

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,28 @@
-import React, {lazy, Suspense, useEffect} from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 // pages
 import LandingPage from "./pages/landingPage";
 // import Dashboard from "./pages/dashboard/dashboard";
 import PageNotFound from "./pages/PageNotFound";
+import Loading from "./components/Loading";
 
 // component
 import Login from "./pages/auth/login";
 import Signup from "./pages/auth/signup";
 
-import {Routes, Route, Navigate} from "react-router-dom";
-import {useSelector, useDispatch} from "react-redux";
-import {getUserOnLoad} from "./features/user/userReducer";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserOnLoad } from "./features/user/userReducer";
 import jwt_decode from "jwt-decode";
 
 const Dashboard = lazy(() => import("./pages/dashboard/dashboard"));
 const App = () => {
-  const {user, isLoading, isError} = useSelector((state) => state.user);
+  const { user, isLoading, isError } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   // sets state of user from token
   useEffect(() => {
     if (!user && localStorage.getItem("token")) {
-      const {email} = jwt_decode(localStorage.getItem("token"));
+      const { email } = jwt_decode(localStorage.getItem("token"));
       dispatch(getUserOnLoad(email));
     }
   }, []);
@@ -54,6 +55,7 @@ const App = () => {
           )}
           <Route path="/dashboard/*" element={<Dashboard />} />
           <Route path="/404" element={<PageNotFound />} />
+          <Route path="/loading" element={<Loading />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Suspense>
--- front-end/src/components/Loading.jsx
+++ front-end/src/components/Loading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <section class="loading">
+      <div class="circle-one"></div>
+      <div class="circle-two"></div>
+      <span>loading...</span>
+    </section>
+  );
 };
 
 export default Loading;
