863c8cf98ac539af5dea4e490da8c9c12f053116
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-12-08 21:50:40 +0800

    update

--- front-end/src/components/coordinator/ApprovalIntern.jsx
+++ front-end/src/components/coordinator/ApprovalIntern.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const ApprovalIntern = () => {
-  return <div className="approval-intern">ApprovalIntern</div>;
+  const {
+    user: { firstName, lastName },
+  } = useSelector((state) => state.user);
+
+  return (
+    <div className="approval-intern">
+      <div className="img-container"></div>
+      <div className="intern-details">
+        <p className="name">
+          {firstName} {lastName}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ApprovalIntern;
