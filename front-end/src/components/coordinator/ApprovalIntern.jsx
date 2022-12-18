8b460e431fb4469819d19262aa8880dc43c6e99a
Eco_26 <106471939+Eco26@users.noreply.github.com>
2022-12-18 19:52:36 +0800

    update

--- front-end/src/components/coordinator/ApprovalIntern.jsx
+++ front-end/src/components/coordinator/ApprovalIntern.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { FaAngleDoubleDown } from "react-icons/fa";
+import { IoMdNotifications } from "react-icons/io";
 
-const ApprovalIntern = () => {
+const ApprovalIntern = React.memo(({ intern }) => {
   const {
     user: { firstName, lastName },
-  } = useSelector((state) => state.user);
+    schoolDetails,
+    internshipDetails,
+    verification: { isVerified, hasSentVerification },
+  } = intern;
 
   return (
     <div className="approval-intern">
-      <div className="img-container"></div>
-      <div className="intern-details">
-        <p className="name">
-          {firstName} {lastName}
-        </p>
+      <div className="img"></div>
+      <div className="details">
+        <div className="internt-name">
+          <p className="name">
+            {firstName} {lastName}
+          </p>
+          <p className="program">Program: {schoolDetails?.program}</p>
+        </div>
+        <div className="intern-infos">
+          <p className="program"> {internshipDetails?.companyName}</p>
+          {/* <p className="program"> {schoolDetails?.program}</p> */}
+        </div>
+        <div className="intern-infos">
+          <span>
+            <IoMdNotifications />
+          </span>
+        </div>
+        {/* <button>
+          <span>
+            <FaAngleDoubleDown />
+          </span>
+        </button> */}
       </div>
     </div>
   );
-};
+});
 
 export default ApprovalIntern;
