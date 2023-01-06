506226c98976f0d30fbd1bf77298beaf4b4a0c1e
Eco_26 <106471939+Eco26@users.noreply.github.com>
2023-01-06 19:13:38 +0800

    update

---
+++ front-end/src/components/coordinator/Internship.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { TbArrowsSort } from "react-icons/tb";
+import { MdFilterList } from "react-icons/md";
+
+const InternshipsComponents = () => {
+  return (
+    <div className="internship">
+      <header className="top">
+        <h2>Looking for Internship?</h2>
+        <p>
+          Are you finding a hard time looking for internship? We got you! SIMS
+          is here to assist you in finding an internship that suits your chosen
+          fieldsh
+        </p>
+      </header>
+      <div className="mid">
+        <div className="left">
+          <button className="sort-by">
+            <span>
+              <TbArrowsSort />
+            </span>
+            Sort-by
+          </button>
+          <button className="filter">
+            <span>
+              <MdFilterList />
+            </span>
+            Filter
+          </button>
+        </div>
+        <div className="right">
+          <span>
+            <BiSearchAlt />
+          </span>
+          <input placeholder="Search" type="text" />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="main-content">
+          <div className="left-img"></div>
+          <div className="right-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+            repellat dolores id saepe qui. Ad ullam a sed! Praesentium omnis
+            minus aliquam facere eum odio dolor saepe, modi dicta illo natus!
+            Atque, obcaecati sint! Quam qui, maiores ex molestias expedita velit
+            labore ab deleniti quos laudantium quo, odio aspernatur harum!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InternshipsComponents;
--- front-end/src/pages/coordinator/Internships.jsx
+++ front-end/src/pages/coordinator/Internships.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Intership from "../../components/coordinator/Internship";
+import { useSelector } from "react-redux";
 
-const Internships = () => {
-  return <div>Internships</div>;
-};
+const Internships = React.memo(() => {
+  const { approvalInterns } = useSelector((state) => state.intern);
+  const renderApprovals = () => {
+    if (!approvalInterns) {
+      return <h1>loading...</h1>;
+    }
+
+    if (approvalInterns.length === 0) {
+      return <h1>No approval request at the moment</h1>;
+    }
+
+    return approvalInterns.map((intern, index) => {
+      return <Intership intern={intern} key={index} index={index} />;
+    });
+  };
+
+  return (
+    <div className="internship-container">
+      <div className="internship-content">{renderApprovals()}</div>
+    </div>
+  );
+});
 
 export default Internships;
