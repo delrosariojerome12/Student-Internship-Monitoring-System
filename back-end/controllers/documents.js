8f1e7ac8a63a4c8a292565b07a2be0bca1cd2be7
naviran0893 <ivancedie0108@gmail.com>
2023-01-12 17:39:24 +0800

    UPDATE 011223

--- back-end/controllers/documents.js
+++ back-end/controllers/documents.js
@@ -3,12 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequest, NotFound } = require("../errors");
 
 const createDocument = async (req, res) => {
-  try {
-    const document = await Document.create(req.body);
-    res.status(201).json({ document });
-  } catch (error) {
-    res.status(201).json({ msg: error });
-  }
+  const document = await Document.create(req.body);
+  res.status(StatusCodes.OK).json({ document });
 };
 
 module.exports = {
--- back-end/models/Document.js
+++ back-end/models/Document.js
@@ -1,23 +1,25 @@
 const mongoose = require("mongoose");
 
 const DocumentSchema = new mongoose.Schema({
-  document: {
-    type: Object,
-    name: {
-      type: String,
-      required: [true, "Please provide name of the document."],
-    },
-    link: {
-      type: String,
-    },
-    path: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
+  //document: {
+  //type: Object,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: {
+    type: String,
+    required: [true, "Please provide name of the document."],
+  },
+  link: {
+    type: String,
+  },
+  path: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Document", DocumentSchema);
