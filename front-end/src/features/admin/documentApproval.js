f5ab53c81a10514678aeead6baa7dcd8fa01f9b5
fallenZeR0 <delrosario12jerome@gmail.com>
2023-02-02 17:54:58 +0800

    update

--- front-end/src/features/admin/documentApproval.js
+++ front-end/src/features/admin/documentApproval.js
@@ -43,6 +43,40 @@ export const getAllVerifiedInterns = createAsyncThunk(
   }
 );
 
+// export const approve = createAsyncThunk(
+//   "/documentApproval/getAllVerifiedInterns",
+//   async (x, {rejectWithValue}) => {
+//     try {
+//       const url = "http://localhost:5000/intern/getAllVerifiedInterns";
+//       const {data: res} = await axios.get(url);
+
+//       console.log(res);
+//       const newInterns = res.interns
+//         .map((item) => {
+//           const {
+//             verification: {isVerified},
+//             documentDetails,
+//           } = item;
+//           if (isVerified && documentDetails.length !== 0) {
+//             const filteredDocumentDetails = documentDetails.filter((docs) => {
+//               const {completion} = docs;
+//               return completion.hasSent && {docs};
+//             });
+//             return {...item, documentDetails: filteredDocumentDetails};
+//           }
+//         })
+//         .filter((item) => {
+//           return item;
+//         });
+
+//       return {res: newInterns, totalDocuments: res.totalDocuments};
+//     } catch (error) {
+//       console.log(error);
+//       return rejectWithValue(error.response.data);
+//     }
+//   }
+// );
+
 export const documentApprovalReducer = createSlice({
   name: "documentApproval",
   initialState,
