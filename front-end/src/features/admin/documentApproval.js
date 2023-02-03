bf8ec683a033ba4067cfa54ff7cc1246920d9755
fallenZeR0 <delrosario12jerome@gmail.com>
2023-02-03 17:44:32 +0800

    update-not complete

--- front-end/src/features/admin/documentApproval.js
+++ front-end/src/features/admin/documentApproval.js
@@ -4,8 +4,12 @@ import axios from "axios";
 const initialState = {
   isLoading: false,
   isError: false,
+  isSentLoading: false,
+  isSentError: false,
+  isDocumentOpen: false,
   interns: null,
   selectedIntern: null,
+  selectedDocument: null,
   totalDocuments: null,
 };
 
@@ -16,7 +20,6 @@ export const getAllVerifiedInterns = createAsyncThunk(
       const url = "http://localhost:5000/intern/getAllVerifiedInterns";
       const {data: res} = await axios.get(url);
 
-      console.log(res);
       const newInterns = res.interns
         .map((item) => {
           const {
@@ -43,44 +46,65 @@ export const getAllVerifiedInterns = createAsyncThunk(
   }
 );
 
-// export const approve = createAsyncThunk(
-//   "/documentApproval/getAllVerifiedInterns",
-//   async (x, {rejectWithValue}) => {
-//     try {
-//       const url = "http://localhost:5000/intern/getAllVerifiedInterns";
-//       const {data: res} = await axios.get(url);
+export const approveDocumentRequest = createAsyncThunk(
+  "/documentApproval/approveDocument",
+  async ({email, id, documentDetails}, {rejectWithValue}) => {
+    const newDocumentDetails = [...documentDetails]
+      .filter((item) => item._id === id)
+      .map((item) => {
+        const {document, completion} = item;
+        return {
+          completion: {
+            ...completion,
+            isApproved: true,
+            isRejected: false,
+          },
+          document,
+          _id: id,
+        };
+      });
+    const allDocuments = [...documentDetails];
+    // const completeDocumentDetails = [...allDocuments, newDocumentDetails[0]];
 
-//       console.log(res);
-//       const newInterns = res.interns
-//         .map((item) => {
-//           const {
-//             verification: {isVerified},
-//             documentDetails,
-//           } = item;
-//           if (isVerified && documentDetails.length !== 0) {
-//             const filteredDocumentDetails = documentDetails.filter((docs) => {
-//               const {completion} = docs;
-//               return completion.hasSent && {docs};
-//             });
-//             return {...item, documentDetails: filteredDocumentDetails};
-//           }
-//         })
-//         .filter((item) => {
-//           return item;
-//         });
+    console.log(allDocuments);
 
-//       return {res: newInterns, totalDocuments: res.totalDocuments};
-//     } catch (error) {
-//       console.log(error);
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
+    // try {
+    // const url = `http://localhost:5000/intern/approveDocument/${email}`;
+    // const {data: res} = await axios.patch(url, {
+    // documentDetails: completeDocumentDetails,
+    // });
+    // console.log(res.documentDetails);
+    // } catch (error) {
+    // console.log(error);
+    // return rejectWithValue(error.response.data);
+    // }
+  }
+);
+
+export const rejectDocumentRequest = createAsyncThunk(
+  "/documentApproval/rejectDocument",
+  async (x, {rejectWithValue}) => {
+    console.log("reject");
+    try {
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const documentApprovalReducer = createSlice({
   name: "documentApproval",
   initialState,
-  reducers: {},
+  reducers: {
+    handleOpenDocument: (state, action) => {
+      state.isDocumentOpen = true;
+      state.selectedDocument = action.payload;
+    },
+    handleCloseDocument: (state, action) => {
+      state.isDocumentOpen = false;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getAllVerifiedInterns.pending, (state, action) => {
@@ -95,9 +119,32 @@ export const documentApprovalReducer = createSlice({
         state.isLoading = false;
         state.isError = true;
       });
+    builder
+      .addCase(approveDocumentRequest.pending, (state, action) => {
+        state.isSentLoading = true;
+      })
+      .addCase(approveDocumentRequest.fulfilled, (state, action) => {
+        state.isSentLoading = false;
+      })
+      .addCase(approveDocumentRequest.rejected, (state, action) => {
+        state.isSentLoading = false;
+        state.isSentError = true;
+      });
+    builder
+      .addCase(rejectDocumentRequest.pending, (state, action) => {
+        state.isSentLoading = true;
+      })
+      .addCase(rejectDocumentRequest.fulfilled, (state, action) => {
+        state.isSentLoading = false;
+      })
+      .addCase(rejectDocumentRequest.rejected, (state, action) => {
+        state.isSentLoading = false;
+        state.isSentError = true;
+      });
   },
 });
 
-export const {} = documentApprovalReducer.actions;
+export const {handleOpenDocument, handleCloseDocument} =
+  documentApprovalReducer.actions;
 
 export default documentApprovalReducer.reducer;
