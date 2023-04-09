337dc3a2e97861454822949863411f5d64f2eaca
Eco_26 <Jericobalisi26@gmail.com>
2023-04-09 19:18:59 +0800

    update

--- front-end/src/pages/coordinator/Approvals.jsx
+++ front-end/src/pages/coordinator/Approvals.jsx
@@ -1,12 +1,14 @@
-import React, {useEffect} from "react";
+/** @format */
+
+import React, { useEffect } from "react";
 import Approval from "../../components/coordinator/ApprovalIntern";
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import ApprovalWaiting from "../../assets/img/waiting.svg";
 import Bouncing from "../../components/loading/Bouncing";
 import ServerError from "../serverError";
-import {getAllInterns} from "../../features/interns/internReducer";
+import { getAllInterns } from "../../features/interns/internReducer";
 const Approvals = React.memo(() => {
-  const {approvalInterns, isLoading, isError} = useSelector(
+  const { approvalInterns, isLoading, isError } = useSelector(
     (state) => state.intern
   );
   const dispatch = useDispatch();
@@ -20,24 +22,24 @@ const Approvals = React.memo(() => {
       return <h1>loading...</h1>;
     }
 
-    if (approvalInterns.length === 0) {
-      return (
-        <section className="approvals">
-          <div className="no-entries">
-            <h3>
-              Oops, there were no <b>entries</b> yet come back again later
-            </h3>
-            <div className="img-waiting">
-              <img src={ApprovalWaiting} alt="Approvals waiting image" />
-            </div>
+    // if (approvalInterns.length === 0) {
+    return (
+      <section className="approvals">
+        <div className="no-entries">
+          <h3>
+            Oops, there were no <b>entries</b> yet come back again later
+          </h3>
+          <div className="img-waiting">
+            <img src={ApprovalWaiting} alt="Approvals waiting image" />
           </div>
-        </section>
-      );
-    }
+        </div>
+      </section>
+    );
+    // }
 
-    return approvalInterns.map((intern, index) => {
-      return <Approval intern={intern} key={index} index={index} />;
-    });
+    // return approvalInterns.map((intern, index) => {
+    //   return <Approval intern={intern} key={index} index={index} />;
+    // });
   };
 
   if (isLoading) {
